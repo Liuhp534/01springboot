77df6002f58d471fd30b322f3cb17d4f787e2102
hz16092620 <liuhuipeng@huize.com>
2019-03-07 11:44:03 +0800

    测试加文件和更新文件颜色的变化。

---
+++ src/main/java/cn/liu/AddTest.java
@@ -0,0 +1,12 @@
+package cn.liu;
+
+/**
+ * @description: addtest 新增文件是红色的，git-add之后是绿色的，修改git中的文件变成蓝色
+ * @author: hz16092620
+ * @create: 2019-03-07 11:41
+ */
+public class AddTest {
+    public static void main(String[] args) {
+        System.out.println("add test");
+    }
+}
--- src/main/java/cn/liu/Application.java
+++ src/main/java/cn/liu/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @description: 启动类
+ * @description: 启动类 a
  * @author: hz16092620
  * @create: 2019-03-07 10:56
  */
