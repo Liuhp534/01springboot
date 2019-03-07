0cc7e5b4d977f4241d6d78fe63c2045dd760f879
hz16092620 <liuhuipeng@huize.com>
2019-03-07 11:32:42 +0800

    01springboot初始化。

---
+++ src/main/java/cn/liu/Application.java
@@ -0,0 +1,17 @@
+package cn.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 启动类
+ * @author: hz16092620
+ * @create: 2019-03-07 10:56
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/cn/liu/HelloSpringBoot.java
@@ -0,0 +1,28 @@
+package cn.liu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: hello类
+ * @author: hz16092620
+ * @create: 2019-03-07 10:51
+ */
+@RestController
+public class HelloSpringBoot {
+
+
+    @RequestMapping("helloBoot")
+    public Map<String, Object> sayHello(@RequestParam(value="name", defaultValue="World")String name) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", name);
+        return map;
+    }
+}
